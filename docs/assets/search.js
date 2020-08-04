e97d0d58f72fc574a0926599d9cb4f1f24648427
eeshopjp <69183076+eeshopjp@users.noreply.github.com>
2020-08-04 15:35:08 +0900

    search
    sss

--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,9 +1,29 @@
-$(document).ready(function(){
-  $("#searchbox").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    console.log(value);
+
+  const debounce = (func, wait) => {
+   let timeout;
+   return function executedFunction(...args) {
+     const later = () => {
+       // null timeout to indicate the debounce ended
+       timeout = null;
+       // Execute the callback
+       func(...args);
+     };
+     clearTimeout(timeout);
+     timeout = setTimeout(later, wait);
+   };
+ };
+
+
+$(document).ready(function(e){
+  var returnedFunction = debounce(function() {
+    const value = $("#searchbox").val().toLowerCase();
+    const tbl = $("#price-table").DataTable();
+    
     $("#price-table tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
-  });
+    tbl.draw();
+  }, 250);
+  
+  $("#searchbox").on("keyup change clear", returnedFunction);
 });
